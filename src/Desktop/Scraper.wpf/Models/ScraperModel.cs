0ce771455ece0c0648c6a4d9744acd23e7f36168
aliabp <ali.abp@outlook.com>
2024-05-26 11:25:19 -0700

    Add scraper model to encapsulate scraper attributes

---
+++ src/Desktop/Scraper.wpf/Models/ScraperModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Scraper.wpf.Models;
+
+public class ScraperModel
+{
+    // url which should find in search result
+    public string Url { get; set; }
+
+    // keywords for search
+    public string Keywords { get; set; }
+}
