a5405e4a86c5302a10d98a0f56824b250189717e
aliabp <ali.abp@outlook.com>
2024-05-26 11:28:48 -0700

    Add validation rule for parsing Keywords
    Keywords validation implemented to prevent invalid input from users.

---
+++ src/Desktop/Scraper.wpf/Validation/KeywordsValidation.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Scraper.wpf.Validation;
+
+public class KeywordsValidation : ValidationRule
+{
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    {
+
+        string? keywords = value as string;
+
+        // check keywords input lenght is between 3 to 100
+        if (keywords == null || keywords.Length < 3 || keywords.Length > 100)
+        {
+            return new ValidationResult(false, "The keywords field is out of 3-100.");
+        }
+
+        return ValidationResult.ValidResult;
+    }
+
+}
--- src/Desktop/Scraper.wpf/Validation/UrlValidation.cs
+++ src/Desktop/Scraper.wpf/Validation/UrlValidation.cs
@@ -1,36 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
+﻿using System.Globalization;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Controls;
 
-namespace Scraper.wpf.Validation
+namespace Scraper.wpf.Validation;
+
+public class UrlValidation : ValidationRule
 {
-    public class UrlValidation : ValidationRule
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
     {
-        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        // standard URL regular expression
+        var urlRegex = new Regex(
+                            @"^(https?)" +
+                            @":\/\/" +
+                            @"(www\.)?" +
+                            @"([a-zA-Z0-9\-]+\.)+" +
+                            "[a-zA-Z]{2,}" +
+                            @"(\/[a-zA-Z0-9?=&]+)*\/?$",
+                            RegexOptions.IgnoreCase);
+
+        string? url = value as string;
+
+        // check URL is valid and well format
+        if (url == null || !urlRegex.IsMatch(url))
         {
-            var urlRegex = new Regex(
-                                @"^(https?)" +
-                                @":\/\/" +
-                                @"(www\.)?" +
-                                @"([a-zA-Z0-9\-]+\.)+" +
-                                "[a-zA-Z]{2,}" +
-                                @"(\/[a-zA-Z0-9?=&]+)*\/?$",
-                                RegexOptions.IgnoreCase);
-
-            string? url = value as string;
-
-            if (url == null || !urlRegex.IsMatch(url))
-            {
-                return new ValidationResult(false, "The Url field must be a valid URI.");
-            }
-
-            return ValidationResult.ValidResult;
+            return new ValidationResult(false, "The Url field must be a valid URI.");
         }
 
+        return ValidationResult.ValidResult;
     }
+
 }
